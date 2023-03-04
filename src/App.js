5f9d4c64f5f808ece302237297a05f359c83314c
Bhanu Priya <er.priyabhanu@gmail.com>
2023-03-04 19:27:30 +0530

    warning logo removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Profile from './profile';
 
@@ -6,20 +6,6 @@ function App() {
   return (
     <div className="App">
       <Profile/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
